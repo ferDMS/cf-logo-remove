943a5e10ecc148e97ede6f58beb0bff84a5ef917
Daniel Monroy <fernandodms04@gmail.com>
2025-01-14 14:59:30 -0600

    Modular Code

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
-import fitz  # PyMuPDF
+import fitz
 import os
-import re
-
-path_dir = "NOVIEMBRE 2024"
-output_dir = f"final/{path_dir}"
+import sys
 
 # Coordinates for the white rectangle (x0, y0, x1, y1)
-rect_coords = (450, 25, 600, 55)
+RECT_COORDS = (450, 25, 600, 55)
 
-for file in os.listdir(path_dir):
-    filename = file.split('.')[0]
-    print(f"Processing file: {filename}")
-    path_file = os.sep.join([path_dir, f"{filename}.pdf"])
-    with fitz.open(path_file) as doc:
-        if not filename.endswith('-'):
-            print(f"Modifying file: {filename}")
-            # Draw a white rectangle on the first page
-            first_page = doc[0]
-            rect = fitz.Rect(*rect_coords)
-            first_page.draw_rect(rect, color=(1, 1, 1), fill=(1, 1, 1))
-        else:
-            print(f"Filename ends with '-': {filename}")
-            filename = filename.rstrip('-')
-        output_path = os.sep.join([output_dir, f"{filename}.pdf"])
-        doc.save(output_path)
-        print(f"Saved file to: {output_path}")
+def parseFromDirectory(input_dir, output_dir):
+    for file in os.listdir(input_dir):
+        filename = file.split('.')[0]
+        print(f"Processing file: {filename}")
+        path_file = os.sep.join([input_dir, f"{filename}.pdf"])
+        with fitz.open(path_file) as doc:
+            if not filename.endswith('-'):
+                print(f"Modifying file: {filename}")
+                # Draw a white rectangle on the first page
+                first_page = doc[0]
+                rect = fitz.Rect(*RECT_COORDS)
+                first_page.draw_rect(rect, color=(1, 1, 1), fill=(1, 1, 1))
+            else:
+                print(f"Filename ends with '-': {filename}")
+                filename = filename.rstrip('-')
+            output_path = os.sep.join([output_dir, f"{filename}.pdf"])
+            doc.save(output_path)
+            print(f"Saved file to: {output_path}")
 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <input_directory>")
+        sys.exit(1)
+
+    input_dir = sys.argv[1]
+    output_dir = os.sep.join(["final", input_dir])
+
+    if os.path.exists(output_dir):
+        counter = 1
+        new_output_dir = output_dir
+        while os.path.exists(new_output_dir):
+            new_output_dir = f"{output_dir} ({counter})"
+            counter += 1
+        output_dir = new_output_dir
+    
+    os.makedirs(output_dir)
+
+    parseFromDirectory(input_dir, output_dir)
